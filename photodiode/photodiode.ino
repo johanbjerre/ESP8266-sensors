1de1b2b5f44c450d458f322cc5489e98f585dfe5
root <jubjerre@gmail.com>
2019-11-08 21:53:10 +0100

    testing diode

--- photodiode/photodiode.ino
+++ photodiode/photodiode.ino
@@ -1,31 +1,18 @@
 #include <ESP8266WiFi.h>
 
 
-#define MIN_VALUE 25
-#define MAX_VALUE 800
 #define SENSOR_PIN A0    // select the input pin for the potentiometer
 
-int sensorValue1 = 0; 
-int sensorValue2 = 0; 
+int sensorValue = 0;
 
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-  sensorValue1 = analogRead(SENSOR_PIN);
-  sensorValue2 = digitalRead(SENSOR_PIN);
+  sensorValue = analogRead(SENSOR_PIN);
   Serial.print("analog:");
-  Serial.print(sensorValue1);
-  Serial.print(" digital:");
-  Serial.print(sensorValue2);
-  Serial.print(" ");
-  float level = 100 - ((sensorValue1 - MIN_VALUE) * 100 / (MAX_VALUE - MIN_VALUE));
+  Serial.println(sensorValue);
   
-  sensorValue1 = level;
-  Serial.print("LUX:");
- Serial.println(sensorValue1);
-
-
   delay(100);
 }
