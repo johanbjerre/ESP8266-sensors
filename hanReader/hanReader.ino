fd06679e4f0128e7d4ddae6fcb015df2614e93b3
Johan Bjerre <Johan.Bjerre@ist.com>
2023-03-11 20:16:08 +0100

    test esp

--- hanReader/hanReader.ino
+++ hanReader/hanReader.ino
@@ -1,19 +1,28 @@
-
+#include <Ticker.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
+// Wifi
 WiFiClient client;
+const char *SSID = "";
+const char *PASSWORD = "";
+
+const int DELAY_TIME = 5000; //1000*60*5=300000 (5 min)
+
+// Ticker
+Ticker ticker;
 
 void setup()
 {
   Serial.begin(9600);
   //LED set as output
-  pinMode(LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   //TURN OFF LED
-  digitalWrite(LED, HIGH);
+  //digitalWrite(LED, HIGH);
 
+  ticker.attach(0.2,tick);
   setupWifi();
 }
 
@@ -22,12 +31,12 @@ void setupWifi()
   Serial.begin(9600);
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(SSID);
+  Serial.println(SSID); 
 
   WiFi.begin(SSID, PASSWORD);
   WiFi.mode(WIFI_STA);
 
-  while (WiFi.status() != WL_CONNECTED)
+  while (WiFi.status() != WL_CONNECTED) 
   {
     delay(10000);
     Serial.print(".");
@@ -38,7 +47,12 @@ void setupWifi()
 void loop()
 {
   Serial.println();
+  ticker.detach();
   delay(DELAY_TIME);
 }
 
-
+void tick()
+{
+    int state = digitalRead(LED_BUILTIN);    // * Get the current state of GPIO1 pin
+    digitalWrite(LED_BUILTIN, !state);       // * Set pin to the opposite state
+}
