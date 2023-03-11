01ed32589c83b17540fb40ce714761bb0b4ce2b0
Johan Bjerre <Johan.Bjerre@ist.com>
2023-03-11 20:41:01 +0100

    test esp

--- hanReader/hanReader.ino
+++ hanReader/hanReader.ino
@@ -8,13 +8,15 @@ WiFiClient client;
 const char *SSID = "";
 const char *PASSWORD = "";
 
-const int DELAY_TIME = 5000; //1000*60*5=300000 (5 min)
+const int DELAY_TIME = 5000;
+const int DELAY_AFTER_INIT = 10000;
+const int UPDATE_INTERVAL = 20000;
+long LAST_UPDATE_DATA = millis();
 
 // Ticker
 Ticker ticker;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   //LED set as output
   pinMode(LED_BUILTIN, OUTPUT);
@@ -22,37 +24,43 @@ void setup()
   //TURN OFF LED
   //digitalWrite(LED, HIGH);
 
-  ticker.attach(0.2,tick);
+  ticker.attach(0.2, tick);
   setupWifi();
 }
 
-void setupWifi()
-{
+void setupWifi() {
   Serial.begin(9600);
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(SSID); 
+  Serial.println(SSID);
 
   WiFi.begin(SSID, PASSWORD);
   WiFi.mode(WIFI_STA);
 
-  while (WiFi.status() != WL_CONNECTED) 
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(10000);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
-}
-void loop()
-{
-  Serial.println();
   ticker.detach();
-  delay(DELAY_TIME);
+}
+void loop() {
+  long now = millis();
+  Serial.println("loop now:" + String(now) + ", LAST_UPDATE_DATA:" + String(LAST_UPDATE_DATA) + ", UPDATE_INTERVAL:" + String(UPDATE_INTERVAL));
+  if (now - LAST_UPDATE_DATA > UPDATE_INTERVAL) {
+    readData();
+  } else {
+    Serial.println("readData NO");
+  }
 }
 
-void tick()
-{
-    int state = digitalRead(LED_BUILTIN);    // * Get the current state of GPIO1 pin
-    digitalWrite(LED_BUILTIN, !state);       // * Set pin to the opposite state
+void tick() {
+  int state = digitalRead(LED_BUILTIN);  // * Get the current state of GPIO1 pin
+  digitalWrite(LED_BUILTIN, !state);     // * Set pin to the opposite state
+}
+
+void readData() {
+  Serial.println("readData YES");
+  LAST_UPDATE_DATA = millis();
 }
