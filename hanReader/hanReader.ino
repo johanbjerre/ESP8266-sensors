5193981ac51a6227013b1841ba50032b828aff1d
Johan Bjerre <Johan.Bjerre@ist.com>
2023-04-03 21:51:46 +0200

    invert rx uart

--- hanReader/hanReader.ino
+++ hanReader/hanReader.ino
@@ -21,7 +21,16 @@ Ticker ticker;
 
 
 void setup() {
-  Serial.begin(9600);
+  // Setup a hw serial connection for communication with the P1 meter and logging (not using inversion)
+  Serial.begin(9600, SERIAL_8N1, SERIAL_FULL);
+  Serial.println("");
+  Serial.println("Swapping UART0 RX to inverted");
+  Serial.flush();
+
+  // Invert the RX serialport by setting a register value, this way the TX might continue normally allowing the serial monitor to read println's
+  USC0(UART0) = USC0(UART0) | BIT(UCRXI);
+  Serial.println("Serial port is ready to recieve.");
+  
   pinMode(LED_BUILTIN, OUTPUT);
 
   ticker.attach(0.2, tick);
